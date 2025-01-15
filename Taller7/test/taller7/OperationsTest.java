ccf0778204f2b83d4e4a95cbff0c806f1be664a3
wamucant <wamucant@espol.edu.ec>
2025-01-15 16:30:46 -0500

    commitFormula

--- Taller7/test/taller7/OperationsTest.java
+++ Taller7/test/taller7/OperationsTest.java
@@ -11,4 +11,18 @@ class OperationsTest {
         String result = Operations.Solve("34-80+1");
         assertEquals("34-80+1=-45", result, "La fórmula no se resolvió correctamente.");
     }
+
+    @Test
+    void testWithInvalidFormula() {
+        Exception exception = assertThrows(NumberFormatException.class, () -> Operations.Solve("3++5"));
+        assertTrue(exception.getMessage().contains("For input string"), "No se lanzó la excepción esperada para fórmula inválida.");
+    }
+
+    @Test
+    void testWithNullFormula() {
+        String formula = null;
+        assertThrows(NullPointerException.class, () -> { Operations.Solve(formula); }, "La función Solve no debería ser null."); 
+    }
+
+
 }
